f77f3fbf2d491c857107b0cac6d1cffe9df397fa
eskaine <eskainesper@gmail.com>
2024-07-13 13:11:35 +0800

    chore: added navbar to homepage

---
+++ client/src/components/layouts/Navbar.tsx
@@ -0,0 +1,15 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+export default function Navbar() {
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <div className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Simple Web App</span>
+                </div>
+                <ConnectButton />
+            </div>
+        </nav>
+    )
+}
