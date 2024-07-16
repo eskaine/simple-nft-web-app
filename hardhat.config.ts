72e13b03d8e6c6c9b06e55f19f75e4fb3fbef0d3
eskaine <eskainesper@gmail.com>
2024-07-16 19:15:55 +0800

    chore: added verify script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,6 +27,16 @@ const config: HardhatUserConfig = {
       sepolia: ETHERSCAN_API_KEY!,
       polygonAmoy: POLYGONSCAM_API_KEY!,
     },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com"
+        },
+      }
+    ]
   },
 };
 
